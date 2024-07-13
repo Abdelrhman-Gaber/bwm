8a2a88c84e250f9b54d46f560376c66edb7b93b5
Abdelrhman Gaber Youssef <gaberabdo68@gmail.com>
2024-07-13 12:55:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
-import numpy as np
 import pandas as pd
-import streamlit as st 
 import joblib
-
-regressor=joblib.load('model_dep.pkl')
-
-
-def welcome():
-    return "Welcome All"
+import numpy as np
+import streamlit as st
 
 
-def predict_bmwcar(model,	year,	transmission,	mileage	,fuelType,	tax	,mpg,	engineSize):
-  prediction=regressor.predict(pd.DataFrame({'model':[model],'year':[year]	,'transmission':[transmission],	'mileage':[mileage],	'fuelType':[fuelType],'tax':[tax],	'mpg':[mpg],	'engineSize':[engineSize]}))
-  print(prediction)
-  return  prediction
+regressor = joblib.load('model_dep.pkl')
+df = pd.read_csv('bmw.csv')
+
+def predict_bmw_price(model, year, transmission, mileage,fuelType ,tax, mpg, engineSize):
+    prediction = regressor.predict(pd.DataFrame({'model':[model],'year':[year]	,'transmission':[transmission],	'mileage':[mileage],'fuelType':[fuelType],'tax':[tax],	'mpg':[mpg],'engineSize':[engineSize]}))
+    return prediction
+
 def main():
-    st.title("BMW price prediction")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit BMW price prediction App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    model = st.text_input("model","Type Here")
-    year = st.text_input("year","Type Here")
-    transmission = st.text_input("transmission","Type Here")
-    mileage = st.text_input("mileage","Type Here")
-    fuelType = st.text_input("fuelType","Type Here")
-    tax = st.text_input("tax","Type Here")
-    mpg = st.text_input("mpg","Type Here")
-    engineSize = st.text_input("engineSize","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=predict_bmwcar(model,	year,	transmission,	mileage	,fuelType,	tax	,mpg,	engineSize)
-    st.success('The price is {} USD'.format(result))
-    if st.button("About"):
-        st.text("our app using streamlit")
-        st.text("best of luck in your GP")
+    st.title('BMW price prediction')
+    
+    model = st.selectbox("model" , df['model'].unique())
+    year = st.text_input('year', 'please enter the year')
+    transmission = st.selectbox("transmission" , df['transmission'].unique())
+    mileage = st.text_input('mileage', 'please enter the mileage')
+    fuelType = st.selectbox("fuelType" , df['fuelType'].unique())
+    tax = st.text_input('tax', 'please enter the tax')
+    mpg = st.text_input('mpg', 'please enter the mpg')
+    engineSize = st.text_input('engineSize', 'please enter the engineSize')
+    result = ""
+    if st.button('predict'):
+        result = predict_bmw_price(model, year, transmission, mileage,fuelType ,tax, mpg, engineSize)
+    st.success('the price of the car is {} USD'.format(result))
+    
 if __name__=='__main__':
-    main()        
+    main()
